f271ea2dd07bad7ace31f59f8390445b9421f07d
김준태 <bexellenting@gmail.com>
2018-01-31 17:28:20 +0900

    chp.4's exercises for v-for directive

---
+++ 4. List_rendering/practice.js
@@ -0,0 +1,12 @@
+var listPractice = new Vue({
+  el: '.container',
+  data: {
+    person:{
+      name: "Kostas",
+      height: "1.73ㅜm",
+      weight: "65kg",
+      eyeColor: "brown",
+      favoriteFood: "Ntolmas"
+    }
+  }
+})
