38ec09d35289a1e00a719933a52ed18dc220d7ae
김준태 <bexellenting@gmail.com>
2018-01-31 17:22:48 +0900

    simple exmaple for v-for directive

---
+++ 4. List_rendering/class2.js
@@ -0,0 +1,10 @@
+const list2 = new Vue({
+  el: '.container',
+  data: {
+    stories: [
+      "i crashed my car today!",
+      "Yesterday, someone sole my bag!",
+      "Someone ate my chocolate",
+    ]
+  }
+})
