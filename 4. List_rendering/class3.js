5808c879782121d51d7482056f1a5a65fdd27365
김준태 <bexellenting@gmail.com>
2018-01-31 17:25:21 +0900

    Traversing the object array

---
+++ 4. List_rendering/class3.js
@@ -0,0 +1,23 @@
+const list3 = new Vue({
+  el: '.container',
+  data: {
+    stories: [
+      {
+        plot: "I crashed my car today!",
+        writer: "Alex"
+      },
+      {
+        plot: "Yesterday, someone sole my bag!",
+        writer: "John"
+      },
+      {
+        plot: "Someone ate my chocolate",
+        writer: "Atom"
+      },
+      {
+        plot: "i ate Someone's chocolate",
+        writer: "Alex"
+      },
+    ]
+  }
+})
